78e58dd555fcc792177c71feac5147e276b66c59
Daniel Carrion <daniel.carrion@harrison.ai>
2022-07-01 13:43:37 +1000

    fix format

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "queue_arn" {
   description = "Queue arn."
-  value = aws_sqs_queue.queue[0].arn
+  value       = aws_sqs_queue.queue[0].arn
 }
 
 output "queue_url" {
   description = "Queue url."
-  value = aws_sqs_queue.queue[0].url
+  value       = aws_sqs_queue.queue[0].url
 }
 
 output "dl_queue_arn" {
   description = "Dead letter queue arn."
-  value = aws_sqs_queue.dlqueue[0].arn
+  value       = aws_sqs_queue.dlqueue[0].arn
 }
 
 output "dl_queue_url" {
   description = "Dead letter queue url."
-  value = aws_sqs_queue.dlqueue[0].url
+  value       = aws_sqs_queue.dlqueue[0].url
 }
