6df144c603861b473f3e507272d42576ae724c1f
peasee <98815791+peasee@users.noreply.github.com>
2023-01-19 14:40:41 +1000

    feat: Add lambda function ARN and name to outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,13 @@ output "dl_queue_url" {
   description = "Dead letter queue url."
   value       = one(aws_sqs_queue.dlqueue[*].url)
 }
+
+output "lambda_function_arn" {
+  description = "Lambda function arn."
+  value       = one(aws_lambda_function.this.lambda_function_arn)
+}
+
+output "lambda_function_name" {
+  description = "Lambda function name."
+  value       = one(aws_lambda_function.this.lambda_function_name)
+}
